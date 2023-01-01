b704d48285b3883b75162ffff938bde951619668
Geoff Hackett <ghackett@gmail.com>
2022-12-31 22:30:40 -0500

    test any(KClass)

--- src/commonTest/kotlin/reflectivemockk/TheHackTest.kt
+++ src/commonTest/kotlin/reflectivemockk/TheHackTest.kt
@@ -3,11 +3,17 @@ package reflectivemockk
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
+import com.episode6.reflectivemockk.any
 import com.episode6.reflectivemockk.findCallRecorder
 import io.mockk.*
 import kotlin.test.Test
 
 class TheHackTest {
+
+  class TestClass {
+    fun withHiFive(input: String): String = "HiFive:$input"
+  }
+
   @Test fun testFindRecorderWorks() {
     val callRecorder = mockk<MockKGateway.CallRecorder>()
     val lambda = CapturingSlot<Function<*>>()
@@ -16,5 +22,15 @@ class TheHackTest {
     val result = scope.findCallRecorder()
 
     assertThat(result).isNotNull().isEqualTo(callRecorder)
+  }
+
+  @Test fun testManualUseOfAny() {
+    val mockTestClass = mockk<TestClass> {
+      every { withHiFive(any(String::class)) } returns "mocked"
+    }
+
+    val result = mockTestClass.withHiFive("some input")
+
+    assertThat(result).isEqualTo("mocked")
   }
 }
