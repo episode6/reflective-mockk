261d222e5f5610311bf03554b67a899f53543f3c
Geoff Hackett <ghackett@gmail.com>
2023-01-02 15:01:00 -0500

    Add RawAPI (#2)

---
+++ src/commonMain/kotlin/com/episode6/reflectivemockk/RawApi.kt
@@ -0,0 +1,43 @@
+package com.episode6.reflectivemockk
+
+import io.mockk.MockKMatcherScope
+import kotlin.reflect.KCallable
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.typeOf
+
+internal fun MockKMatcherScope.reflectiveAny(receiverType: KType, rawParamType: KType): Any {
+  val paramType = receiverType.resolveInnerType(rawParamType)
+  return any(paramType.classifier as KClass<*>)
+}
+
+/**
+ * Allows for reflection-based stubbing of methods.
+ *
+ * Usage example (mock all member functions in one mock):
+ *
+ * val mock = mockk<SomeClass>()
+ * mock.javaClass.kotlin.memberFunctions.forEach {
+ *   every { callTo(it, receiver = mock) } answers { /* some default answer */ }
+ * }
+ */
+public inline fun <reified RECEIVER : Any> MockKMatcherScope.callTo(callable: KCallable<*>, receiver: RECEIVER): Any? =
+  callTo(callable = callable, receiver = receiver, receiverType = typeOf<RECEIVER>())
+
+/**
+ * Allows for reflection-based stubbing of methods.
+ *
+ * Usage example (mock all member functions in one mock):
+ *
+ * val mock = mockk<SomeClass>()
+ * val mockType = typeOf<SomeClass>()
+ * mock.javaClass.kotlin.memberFunctions.forEach {
+ *   every { callTo(it, receiver = mock, receiverType = mockType) } answers { /* some default answer */ }
+ * }
+ */
+public fun MockKMatcherScope.callTo(callable: KCallable<*>, receiver: Any, receiverType: KType): Any? {
+  val params = callable.parameters.drop(1) // first param is always receiver
+    .map { reflectiveAny(receiverType = receiverType, rawParamType = it.type) }
+  val allParams: List<Any> = listOf(receiver) + params
+  return callable.call(*allParams.toTypedArray())
+}
---
+++ src/commonTest/kotlin/reflectivemockk/RawApiTest.kt
@@ -0,0 +1,57 @@
+package reflectivemockk
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.episode6.reflectivemockk.callTo
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import kotlin.reflect.full.memberFunctions
+
+class RawApiTest {
+  interface TestInterface {
+    fun someFunction(input: String): String
+  }
+
+  interface TestGenericInterface<T> {
+    fun someFunction(input: T): T
+  }
+
+  interface TestInterfaceWithGenericFunction {
+    fun <T> someFunction(input: T): T
+  }
+
+  @Test fun testUsageWithSimpleInterface() {
+    val mockTestClass = mockk<TestInterface>()
+    val func = mockTestClass.javaClass.kotlin.memberFunctions.find { it.name == "someFunction" }!!
+    every { callTo(func, receiver = mockTestClass) } returns "mocked"
+
+    val result = mockTestClass.someFunction("something")
+
+    assertThat(result).isEqualTo("mocked")
+    verify { mockTestClass.someFunction("something") }
+  }
+
+  @Test fun testUsageWithGenericInterface() {
+    val mockTestClass = mockk<TestGenericInterface<String>>()
+    val func = mockTestClass.javaClass.kotlin.memberFunctions.find { it.name == "someFunction" }!!
+    every { callTo(func, receiver = mockTestClass) } returns "mocked"
+
+    val result = mockTestClass.someFunction("something")
+
+    assertThat(result).isEqualTo("mocked")
+    verify { mockTestClass.someFunction("something") }
+  }
+
+  @Test fun testUsageWithGenericFunction() {
+    val mockTestClass = mockk<TestInterfaceWithGenericFunction>()
+    val func = mockTestClass.javaClass.kotlin.memberFunctions.find { it.name == "someFunction" }!!
+    every { callTo(func, receiver = mockTestClass) } returns "mocked"
+
+    val result = mockTestClass.someFunction("something")
+
+    assertThat(result).isEqualTo("mocked")
+    verify { mockTestClass.someFunction("something") }
+  }
+}
