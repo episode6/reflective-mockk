faafe0a366760bc52f5283d9859936201bbb1d3d
Geoff Hackett <ghackett@gmail.com>
2023-01-02 14:53:46 -0500

    add comments

--- src/commonMain/kotlin/com/episode6/reflectivemockk/RawApi.kt
+++ src/commonMain/kotlin/com/episode6/reflectivemockk/RawApi.kt
@@ -11,12 +11,33 @@ internal fun MockKMatcherScope.reflectiveAny(receiverType: KType, rawParamType: 
   return any(paramType.classifier as KClass<*>)
 }
 
+/**
+ * Allows for reflection-based stubbing of methods.
+ *
+ * Usage example (mock all member functions in one mock):
+ *
+ * val mock = mockk<SomeClass>()
+ * mock.javaClass.kotlin.memberFunctions.forEach {
+ *   every { callTo(it, receiver = mock) } answers { /* some default answer */ }
+ * }
+ */
+public inline fun <reified RECEIVER : Any> MockKMatcherScope.callTo(callable: KCallable<*>, receiver: RECEIVER): Any? =
+  callTo(callable = callable, receiver = receiver, receiverType = typeOf<RECEIVER>())
+
+/**
+ * Allows for reflection-based stubbing of methods.
+ *
+ * Usage example (mock all member functions in one mock):
+ *
+ * val mock = mockk<SomeClass>()
+ * val mockType = typeOf<SomeClass>()
+ * mock.javaClass.kotlin.memberFunctions.forEach {
+ *   every { callTo(it, receiver = mock, receiverType = mockType) } answers { /* some default answer */ }
+ * }
+ */
 public fun MockKMatcherScope.callTo(callable: KCallable<*>, receiver: Any, receiverType: KType): Any? {
   val params = callable.parameters.drop(1) // first param is always receiver
     .map { reflectiveAny(receiverType = receiverType, rawParamType = it.type) }
   val allParams: List<Any> = listOf(receiver) + params
   return callable.call(*allParams.toTypedArray())
 }
-
-public inline fun <reified RECEIVER : Any> MockKMatcherScope.callTo(callable: KCallable<*>, receiver: RECEIVER): Any? =
-  callTo(callable = callable, receiver = receiver, receiverType = typeOf<RECEIVER>())
