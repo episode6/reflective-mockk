5dca7c242c01053901f28bba7dcd3b429547f09d
Geoff Hackett <ghackett@gmail.com>
2023-01-02 13:45:19 -0500

    clean up new api methods so reflectiveAny can stay internal

--- src/commonMain/kotlin/com/episode6/reflectivemockk/Api.kt
+++ src/commonMain/kotlin/com/episode6/reflectivemockk/Api.kt
@@ -6,20 +6,17 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
-/**
- * Accepts a raw KType (that was found in the context of [RECEIVER]), resolves the type and returns
- * [any] using the resolved type.
- */
-@PublishedApi
 internal fun MockKMatcherScope.reflectiveAny(receiverType: KType, rawParamType: KType): Any {
   val paramType = receiverType.resolveInnerType(rawParamType)
   return any(paramType.classifier as KClass<*>)
 }
 
-public inline fun <reified RECEIVER: Any> MockKMatcherScope.callTo(receiver: RECEIVER, callable: KCallable<*>): Any? {
-  val receiverType = typeOf<RECEIVER>()
+public fun MockKMatcherScope.callTo(receiver: Any, receiverType: KType, callable: KCallable<*>): Any? {
   val params = callable.parameters.drop(1) // first param is always receiver
     .map { reflectiveAny(receiverType = receiverType, rawParamType = it.type) }
   val allParams: List<Any> = listOf(receiver) + params
   return callable.call(*allParams.toTypedArray())
 }
+
+public inline fun <reified RECEIVER : Any> MockKMatcherScope.callTo(receiver: RECEIVER, callable: KCallable<*>): Any? =
+  callTo(receiver, typeOf<RECEIVER>(), callable)
