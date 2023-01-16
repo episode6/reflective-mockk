04c81475d7e954bd6111b0d87348c5f20a40679e
Geoff Hackett <ghackett@gmail.com>
2023-01-16 16:46:26 -0500

    kdoc (and drop the typealiases)

--- src/commonMain/kotlin/com/episode6/reflectivemockk/ConvenienceApi.kt
+++ src/commonMain/kotlin/com/episode6/reflectivemockk/ConvenienceApi.kt
@@ -43,36 +43,100 @@ public inline fun <reified T : Any> reflectiveMockk(
   relaxUnitFun = relaxUnitFun,
 ).apply { reflectiveStubs(stubbing) }
 
-public class ReflectiveStubbing<T : Any>(public val self: T, private val ktype: KType) {
+/**
+ * The receiver used to conveniently define reflective stubs on a mockK.
+ */
+public class ReflectiveStubbing<T : Any>(
 
+  /**
+   * The mockK that reflective stubs are applied to.
+   */
+  public val self: T,
+
+  /**
+   * The ktype of [self], generated using [typeOf]
+   */
+  private val kType: KType,
+) {
+
+  /**
+   * Convenience accessor the [KClass] of [self]. Reflective properties and functions should all come from
+   * this instance and not [kType], since kType might be an interface with inaccessible methods.
+   */
   public val kClass: KClass<*> get() = self.kotlinClass
+
+  /**
+   * Convenience accessor for [kotlin.reflect.full.memberProperties] via [kClass]
+   */
   public val memberProperties: Collection<KProperty1<*, *>> get() = kClass.memberProperties
+
+  /**
+   * Convenience accessor for [kotlin.reflect.full.memberFunctions] via [kClass]
+   */
   public val memberFunctions: Collection<KFunction<*>> get() = kClass.memberFunctions
+
+  /**
+   * Convenience accessor for non-suspendable functions via [kClass]
+   */
   public val normalMemberFunctions: Collection<KFunction<*>> get() = memberFunctions.filter { !it.isSuspend }
+
+  /**
+   * Convenience accessor for suspendable functions via [kClass]
+   */
   public val suspendMemberFunctions: Collection<KFunction<*>> get() = memberFunctions.filter { it.isSuspend }
 
+  /**
+   * Convenience function to filter a collection of [KCallable] by return-type
+   */
   public inline fun <reified R : Any?> Collection<KCallable<*>>.filterReturnType(): Collection<KCallable<*>> =
     filter { it.returnType.classifier == R::class }
 
-  public fun MockKMatcherScope.callTo(callable: KCallable<*>): Any? = callTo(callable, self, ktype)
-  public suspend fun MockKMatcherScope.suspendCallTo(callable: KCallable<*>): Any? = suspendCallTo(callable, self, ktype)
+  /**
+   * Convenience function to reflectively match a call to the given [KCallable].
+   * [callable] must be a normal member function or property of [self].
+   */
+  public fun MockKMatcherScope.callTo(callable: KCallable<*>): Any? = callTo(callable, self, kType)
 
+  /**
+   * Convenience function to reflectively match a call to the given suspendable [KCallable].
+   * [callable] must be a suspendable member function of [self].
+   */
+  public suspend fun MockKMatcherScope.suspendCallTo(callable: KCallable<*>): Any? =
+    suspendCallTo(callable, self, kType)
+
+  /**
+   * Convenience function for every { callTo(callable) }
+   * [callable] must be a normal member function or property of [self].
+   */
   public fun everyCallTo(callable: KCallable<*>): MockKStubScope<Any?, Any?> = every { callTo(callable) }
+
+  /**
+   * Convenience function for coEvery { suspendCallTo(callable) }
+   * [callable] must be a suspendable member function of [self].
+   */
   public fun coEveryCallTo(callable: KCallable<*>): MockKStubScope<Any?, Any?> = coEvery { suspendCallTo(callable) }
 
-  public fun answerEveryCallIn(calls: Collection<KCallable<*>>, answer: AnyAnswer) {
-    calls.forEach { everyCallTo(it) answers (answer) }
-  }
+  /**
+   * Convenience function to apply the same answer to every [KCallable] in [calls]
+   */
+  public fun answerEveryCallIn(
+    calls: Collection<KCallable<*>>,
+    answer: MockKAnswerScope<Any?, Any?>.(Call) -> Any?
+  ) { calls.forEach { everyCallTo(it) answers (answer) } }
 
-  public fun coAnswerEveryCallIn(calls: Collection<KCallable<*>>, answer: CoAnyAnswer) {
-    calls.forEach { coEveryCallTo(it) coAnswers (answer) }
-  }
+  /**
+   * Convenience function to apply the same suspendable answer to [KCallable] in [calls]
+   */
+  public fun coAnswerEveryCallIn(
+    calls: Collection<KCallable<*>>,
+    answer: suspend MockKAnswerScope<Any?, Any?>.(Call) -> Any?
+  ) { calls.forEach { coEveryCallTo(it) coAnswers (answer) } }
 
-  public fun defaultAnswer(answer: AnyAnswer) {
+  /**
+   * Convenience function to apply the same answer to every member property and function in [self]
+   */
+  public fun defaultAnswer(answer: MockKAnswerScope<Any?, Any?>.(Call) -> Any?) {
     answerEveryCallIn(calls = memberProperties + normalMemberFunctions, answer)
     coAnswerEveryCallIn(calls = suspendMemberFunctions, answer)
   }
 }
-
-public typealias AnyAnswer = MockKAnswerScope<Any?, Any?>.(Call) -> Any?
-public typealias CoAnyAnswer = suspend MockKAnswerScope<Any?, Any?>.(Call) -> Any?
